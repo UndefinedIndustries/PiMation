b5f40e5285ae11114bfe8593e28176360900ac85
GT3CH1 <gavinpease@gmail.com>
2017-02-09 13:18:43 -0700

    Added the external webpage module

---
+++ modules/external_webpage/index.ini.php
@@ -0,0 +1,6 @@
+<?php
+ $proto = "https";
+ $server = "ip_of_server";
+ $port = "443";
+ $dir = "path/of/dir/to/file"
+ ?>
---
+++ modules/external_webpage/index.php
@@ -0,0 +1,5 @@
+<?php
+   include_once ('index.ini.php');
+   file_get_contents($proto."://".$server.":".$port."/".$dir);
+
+ ?>
